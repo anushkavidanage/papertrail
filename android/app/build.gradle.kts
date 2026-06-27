d886cd9d9aa9efde9ce3dd045c0797fecbc4123d
Graham Williams <graham.williams@togaware.com>
2026-06-27 10:36:24 +1000

    Avoid using com.example. To be replaced sometime.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.papertrail"
+    namespace = "com.togaware.papertrail"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.papertrail"
+        applicationId = "com.togaware.papertrail"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -25,8 +25,8 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         manifestPlaceholders.putAll(mapOf(
-        "appAuthRedirectScheme" to "com.example.papertrail"
-    )) 
+        "appAuthRedirectScheme" to "com.togaware.papertrail"
+    ))
     }
 
     buildTypes {
